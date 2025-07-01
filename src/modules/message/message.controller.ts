02916541d86ac01e3ad73ce327552c5081a3c1c4
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-07-01 13:12:02 +0500

    fixed message dtos

--- src/modules/message/message.controller.ts
+++ src/modules/message/message.controller.ts
@@ -12,36 +12,44 @@ import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { FindAllMessageDto } from './dto/findAllMessage.dto';
 import { FindAllMessageUserDto } from './dto/findAllMessageUser.dto';
+import { DecoratorWrapper } from 'src/common/auth/decorator.auth';
+import { Role } from 'src/common/auth/roles/role.enum';
 
 @Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
   @Post()
+  @DecoratorWrapper('send Message', true, [Role.Admin])
   create(@Body() createMessageDto: CreateMessageDto) {
     return this.messageService.create(createMessageDto);
   }
 
   @Get()
-  findAll(@Body() dto: FindAllMessageDto) {
+  @DecoratorWrapper('Get Messages')
+  findAll(@Param() dto: FindAllMessageDto) {
     return this.messageService.findAll(dto);
   }
   @Get('/user')
-  findAllUser(@Body() dto: FindAllMessageUserDto) {
+  @DecoratorWrapper('get Message users')
+  findAllUser(@Param() dto: FindAllMessageUserDto) {
     return this.messageService.findAllUser(dto);
   }
 
   @Get('/user/:id')
+  @DecoratorWrapper('get Message user')
   findOneUser(@Param('id', ParseIntPipe) id: string) {
     return this.messageService.findOneUser(+id);
   }
 
   @Get(':id')
+  @DecoratorWrapper('get Message')
   findOne(@Param('id', ParseIntPipe) id: string) {
     return this.messageService.findOne(+id);
   }
 
   @Patch(':id')
+  @DecoratorWrapper('update Message')
   update(
     @Param('id', ParseIntPipe) id: string,
     @Body() updateMessageDto: UpdateMessageDto,
